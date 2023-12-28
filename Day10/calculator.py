1a6d60048d72a07c18f04192a4b2bf86c944492b
eaderogba <eaderogba279@stu.ui.edu.ng>
2023-12-28 19:47:10 +0100

    finalized the code on the calculator task

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,5 +1,8 @@
 # calculator
 # add
+from art import logo
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -29,29 +32,32 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What is the first number?: "))
+
+def calculator():
+    print(logo)
+    num1 = float(input("What is the first number?: "))
+
+    for operators in operations:
+        print(operators)
+
+    should_continue = True
+    while should_continue:
+
+        operation_symbol = input("Pick a operation: ")
+
+        num2 = float(input("What is the next number?: "))
+
+        # Optimized. And a smart way to get it done in just 2 lines of code
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
 
-for operators in operations:
-    print(operators)
-
-operation_symbol = input("Pick a operation from the line above: ")
-
-num2 = int(input("What is the second number?: "))
-
-"""Here is what i first thought of to do the arithmetic after the user enters num1 and num2
-
-if operation_symbol == "+":
-    answer = add(num1, num2)
-elif operation_symbol == "-":
-    answer = subtract(num1, num2)
-elif operation_symbol == "*":
-    answer = multiply(num1, num2)
-elif operation_symbol == "/":
-    answer = divide(num1, num2)"""
-
-# Optimized. And a smart way to get it done in just 2 lines of code
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()

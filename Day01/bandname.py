cbcb431a47b8831bad5831c186bcbd48364a9d35
eaderogba <eaderogba279@stu.ui.edu.ng>
2024-01-06 12:09:05 +0100

    added Day01 Project

---
+++ Day01/bandname.py
@@ -0,0 +1,10 @@
+# 1. Create a greeting for your program.
+print("You're Welcome to the Band Name Generator Program!")
+
+# Program asks the user for the city that they grew up in.
+city_name = input("Which city did you grow up in? \n")
+# Program asks the user for the name of a pet.
+pet_name = input("What is the name of your pet? \n")
+
+# Program combines the name of city and pet, and print band name.
+print(f"The name of your band is {city_name}-{pet_name}")
